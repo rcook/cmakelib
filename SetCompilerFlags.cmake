8d52571318724accdbdfaf108388f4ff9c6e863c
Richard Cook <rcook@rcook.org>
2015-12-24 05:18:27 -0800

    Set default visibility to hidden under GCC

--- SetCompilerFlags.cmake
+++ SetCompilerFlags.cmake
@@ -1,7 +1,7 @@
 function(set_compiler_flags)
   if(MSVC)
   else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -fPIC" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden -g -fPIC" PARENT_SCOPE)
   endif()
 endfunction()
 
